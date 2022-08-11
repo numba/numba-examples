2c80fe3de63546328b88f4c113dd7e51f140f6eb
Ankit Mahato <ankmahato@gmail.com>
2022-08-11 12:11:49 +0530

    Numpy Array Implementtion

---
+++ examples/maths/mandelbrot_set_numpy/numba_prange_numpy_array.py
@@ -0,0 +1,22 @@
+import math
+import numba as nb
+
+@nb.njit(parallel = True)
+def mandelbrot(iters, pixels, bbox, width, max_iter):
+    for y in nb.prange(width):
+        for x in range(width):
+            c0 = complex(bbox[0] + (bbox[2]-bbox[0])*x/width, 
+                         bbox[1] + (bbox[3]-bbox[1])*y/width) 
+            c = 0
+            i = 1
+            while i < max_iter:
+                if abs(c) > 2:  
+                    log_iter = math.log(i) 
+                    pixels[y, x, 0] = 255*(1+math.cos(3.32*log_iter))//2
+                    pixels[y, x, 1] = 255*(1+math.cos(0.774*log_iter))//2
+                    pixels[y, x, 2] = 255*(1+math.cos(0.412*log_iter))//2
+                    break
+                c = c * c + c0
+                i += 1
+            iters[y, x] = i                
+    return iters, pixels
